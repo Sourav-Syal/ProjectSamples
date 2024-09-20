5bc2d0768738fc862f4bed97c1fd89a3366c2cc9
Sourav-Syal <souravsyal0503@gmail.com>
2024-09-20 12:12:33 +0530

    Add new samples

---
+++ InputValues.py
@@ -0,0 +1,8 @@
+Name=input("Enter your Name:")
+Age=int(input("Enter your Age:"))
+Color=input("Enter your Favorite Color:")
+
+#Displaying Inputs
+print("----------------------------")
+print("Your Name is:", Name, "and your age is:", Age, "years old.")
+print("Your Favorite Color is:", Color)
---
+++ Sample1.py
@@ -0,0 +1,7 @@
+Name = "Sourav"
+Age = 26
+Height = 6.1
+
+print("Name:" , Name , " Type:", type(Name))
+print("Age:" , Age , " Type:", type(Age))
+print("Height:" , Height , " Type:", type(Height))
