a249702b84b5e58dbc11dd770b36ebc0c258611a
Sourav-Syal <souravsyal0503@gmail.com>
2024-09-22 16:53:09 +0530

    New program added

--- TurtlePrograms/Circle.py
+++ TurtlePrograms/Circle.py
@@ -1,65 +1,11 @@
 import turtle
 wn = turtle.Screen()
-wn.bgcolor("black")
+beta = turtle.Turtle()
 
-my_turtle = turtle.Turtle()
-my_turtle.shape("blank")
-my_turtle.color("turquoise")
-
-for _ in range(46):
-     my_turtle.speed(10)
-     my_turtle.down()
-     my_turtle.forward(50)
-     my_turtle.stamp()
-     my_turtle.backward(50)
-     my_turtle.right(8)
-
-my_turtle.penup()
-my_turtle.goto(150,150)
-my_turtle.pendown()
-my_turtle.color("green")
-for _ in range(46):
-     my_turtle.speed(10)
-     my_turtle.down()
-     my_turtle.forward(50)
-     my_turtle.stamp()
-     my_turtle.backward(50)
-     my_turtle.right(8)
-
-my_turtle.penup()
-my_turtle.goto(-150,150)
-my_turtle.pendown()
-my_turtle.color("orange")
-for _ in range(46):
-     my_turtle.speed(10)
-     my_turtle.down()
-     my_turtle.forward(50)
-     my_turtle.stamp()
-     my_turtle.backward(50)
-     my_turtle.right(8)
-
-my_turtle.penup()
-my_turtle.goto(-150,-150)
-my_turtle.pendown()
-my_turtle.color("grey")
-for _ in range(46):
-     my_turtle.speed(10)
-     my_turtle.down()
-     my_turtle.forward(50)
-     my_turtle.stamp()
-     my_turtle.backward(50)
-     my_turtle.right(8)
-
-my_turtle.penup()
-my_turtle.goto(150,-150)
-my_turtle.pendown()
-my_turtle.color("white")
-for _ in range(46):
-     my_turtle.speed(10)
-     my_turtle.down()
-     my_turtle.forward(50)
-     my_turtle.stamp()
-     my_turtle.backward(50)
-     my_turtle.right(8)
+beta.speed(10)
+for _ in range(100):
+    for i in range(13):
+        beta.forward(i)
+        beta.left(i)
 
 wn.exitonclick()
