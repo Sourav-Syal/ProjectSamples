a540f335e5b39d32d47332d47fd960230b5a5cd0
Sourav-Syal <souravsyal0503@gmail.com>
2024-09-22 13:54:20 +0530

    Updated code

--- TurtlePrograms/hexagon.py
+++ TurtlePrograms/hexagon.py
@@ -5,6 +5,9 @@ wn.bgcolor("turquoise")
 my_turtle = turtle.Turtle()
 my_turtle.shape("blank")
 my_turtle.pensize(3)
+my_turtle.penup()
+my_turtle.goto(50.00,0.00)
+my_turtle.pendown()
 
 for _ in range(6):
     my_turtle.left(60)
