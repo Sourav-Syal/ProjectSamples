75620f4b273d9a825398b7ce49b6afdb29c99275
Sourav-Syal <souravsyal0503@gmail.com>
2024-10-05 14:33:00 +0530

    New Program Added

--- DatabasesQuery/DBConnection.py
+++ DatabasesQuery/DBConnection.py
@@ -3,7 +3,6 @@ import sqlite3
 con = sqlite3.connect("SQLiteMagic.db")
 cur = con.cursor()
 
-Country = "United States"
 cur.execute('''SELECT * FROM INTERNATIONAL_STUDENT_TEST_SCORES''')
 print(cur.fetchall())
 
